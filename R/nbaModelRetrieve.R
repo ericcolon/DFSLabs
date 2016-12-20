8cfac80d9213ee917d509e08aed709b006978a55
Benjaminryanclarke <benjaminryanclarke@icloud.com>
2016-12-20 04:01:20 -0500

    mm

--- R/nbaModelRetrieve.R
+++ R/nbaModelRetrieve.R
@@ -24,7 +24,7 @@ getNBAPlayerModel <- function(modelDate = NULL, slate = NULL, exclude = NULL,swi
    if (is.null(switch)){
       y <- paste0("~/Documents/NBA",modelDate,".json")
    } else {
-      y <- paste0("~/Documents/NBA",modelDate,switch,".json")
+      y <- paste0("NBA",modelDate,switch,".json")
    }
       pmCurlHandles <- paste0("-H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Referer: http://www.fantasylabs.com/nba/player-models/' -H 'Cookie: ",cookie,"  -o ",y," -H 'Connection: keep-alive' --compressed")
       curlAddress <- paste0("curl ",modelURL," ",pmCurlHandles)
@@ -88,7 +88,8 @@ getNBAPlayerModel <- function(modelDate = NULL, slate = NULL, exclude = NULL,swi
       write.csv(LabsFixedNames2, file = paste0("~/Desktop/NBA_Daily/",modelDate,slate,switch,"_FD.csv"))
     }
  }
-  if(!(is.null(returnFull))){
+
+  if(returnFull=="yes"){
     return(LabsFixedNames1)
   } else {
     return(LabsFixedNames2)
