3b2289d9ab7c1d2191289429ce504b83009dba63
Benjaminryanclarke <benjaminryanclarke@icloud.com>
2016-12-20 04:16:22 -0500

    ok

--- R/getNbaPlayerModel_OtherFunction.R
+++ R/getNbaPlayerModel_OtherFunction.R
@@ -9,7 +9,7 @@
 #'
 #' @examples getNbaPlayerModels_Other(modelDate="12_18_2016",other = "705700",mName = "Russell_and_Flow")
 getNbaPlayerModels_Other <- function(modelDate="12_18_2016",other = "705700",mName = "Russell_and_Flow"){
-  nbaNewModel <- getNBAPlayerModel(modelDate = modelDate ,slate = NULL, exclude = NULL, switch = other, returnFull)
+  nbaNewModel <- getNBAPlayerModel(modelDate = modelDate ,slate = NULL, exclude = NULL, switch = other, returnFull="yes")
   write.csv(nbaNewModel, file = paste0("~/Desktop/NBA_Daily/",modelDate,mName,".csv"))
   newCsvFile <- read.csv(file = paste0("~/Desktop/NBA_Daily/",modelDate,mName,".csv"), stringsAsFactors = FALSE)
   return(newCsvFile)
