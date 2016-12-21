95bfa2b16bcaebe1ff173e1533df01383bccd699
Benjaminryanclarke <benjaminryanclarke@icloud.com>
2016-12-20 21:46:46 -0500

    k

--- R/nbaProjectionsFunction.R
+++ R/nbaProjectionsFunction.R
@@ -1,12 +1,12 @@
 #' NBA Projections
 #'
-#' @param modelDate
+#' @param modelDate 
 #'
 #' @return NBA projections for desired Date
 #' @export
 #'
 #' @examples projectNBA()
-projectNBA <- function(modelDate = NULL, slate = NULL, exclude = NULL, cookie = "__cfduid=d46b64495b55215841566c89ce01d6fbd1480519747; LD_U=http%3A%2F%2Fwww.fantasylabs.com%2F; __distillery=bb3e53d_b333f0ea-b6ab-4d8b-bcbb-e8f78b8d5c3f-86a4ffd60-c4a9ff71f0fe-aef4; .AspNet.Cookies=EVGJH0IuWLy8RuLAWqA3JVB3L8lvyXCaqIDX7mPnHvFo-X76ra1UZRbqo5URKoMo2lrt_BKDiQXclEtWa_hgP7lB6nJsVKo-4dCzLhGJnKZUhq7vWHYc0dHsl2_3m8Wjy-PgYHci5gghAw5y8Lp-rKbi1hRYrHaLQH1_d58PZIZ5q9spxBoCCbZ2ooD-HG0xYfHcPot8_DNrfbeW0XMnByaFVebeNnfem-Mb11nh-wHR3De5peqOHz0-BaRMpG3CnjW8N_pa3OO6BgVMdQ-kr16OSk82gNYDIAAMAvWaqfghUMUjoouY8bJPvqMLcEYX7E2-4K0VWR6usBYAWeZhYOPWPmK9oKhn38Y7RnTawoCNOnqhh6mQyCblU5YwhP7U2xztRCzaNk1N0hvKMvHfQbS35ExGTqeVin7bQIMynh2nYqkjFxhjIT9USeCxQj1zu0UFVZ937dHIARI47-A_QBmier5CJ0Q29n4Isdcu8Bw; flid=-t582kVLjUizx14ZPB2BXQ; __zlcmid=e6g4jX86CRpGap; LD_S=1482140145260; LD_R=; _gat=1; _ga=GA1.2.351079370.1480519759; LD_T=d014f701-2492-4486-bcec-b421e46d54b2'"){
+projectNBA <- function(modelDate = NULL, slate = NULL, exclude = NULL, cookie = "__cfduid=d46b64495b55215841566c89ce01d6fbd1480519747; LD_U=http%3A%2F%2Fwww.fantasylabs.com%2F; __distillery=bb3e53d_b333f0ea-b6ab-4d8b-bcbb-e8f78b8d5c3f-86a4ffd60-c4a9ff71f0fe-aef4; _gat=1; LD_S=1481767246798; LD_R=; .AspNet.Cookies=EVGJH0IuWLy8RuLAWqA3JVB3L8lvyXCaqIDX7mPnHvFo-X76ra1UZRbqo5URKoMo2lrt_BKDiQXclEtWa_hgP7lB6nJsVKo-4dCzLhGJnKZUhq7vWHYc0dHsl2_3m8Wjy-PgYHci5gghAw5y8Lp-rKbi1hRYrHaLQH1_d58PZIZ5q9spxBoCCbZ2ooD-HG0xYfHcPot8_DNrfbeW0XMnByaFVebeNnfem-Mb11nh-wHR3De5peqOHz0-BaRMpG3CnjW8N_pa3OO6BgVMdQ-kr16OSk82gNYDIAAMAvWaqfghUMUjoouY8bJPvqMLcEYX7E2-4K0VWR6usBYAWeZhYOPWPmK9oKhn38Y7RnTawoCNOnqhh6mQyCblU5YwhP7U2xztRCzaNk1N0hvKMvHfQbS35ExGTqeVin7bQIMynh2nYqkjFxhjIT9USeCxQj1zu0UFVZ937dHIARI47-A_QBmier5CJ0Q29n4Isdcu8Bw; flid=-t582kVLjUizx14ZPB2BXQ; __zlcmid=e6g4jX86CRpGap; _ga=GA1.2.351079370.1480519759; LD_T=d014f701-2492-4486-bcec-b421e46d54b2'"){
   getNBAPlayerModel(modelDate,slate,exclude,cookie)
   if (is.null(exclude)) {
     readCSV <- readNBACSVs(modelDate)
@@ -29,13 +29,13 @@ projectNBA <- function(modelDate = NULL, slate = NULL, exclude = NULL, cookie = 
     finalDf <- separate(data = finalDf, col = Projected_Ownership, into = c("minOWN", "maxOWN"))
     finalDf <- na.zero(finalDf)
     finalDfChar <- sapply(finalDf,as.character)
-
+    
     write.csv(finalDfChar, file = paste0("~/Desktop/NBA_Daily/",modelDate,"Projections.csv"))
     scoreQuantiles <- quantile(readCSV$Score)
     scoreThirdQuant <- scoreQuantiles[4]
     finalDf3rdQuartileScoreOnly <- finalDf %>% filter(Score>=scoreThirdQuant) %>% arrange(desc(Projection))
     finalDf3rdQuartileScoreOnlyChar <- sapply(finalDf3rdQuartileScoreOnly,as.character)
-
+    
     write.csv(finalDf3rdQuartileScoreOnlyChar, file = paste0("~/Desktop/NBA_Daily/",modelDate,"Projections3rdQuantileOnly.csv"))
     projValueQuants <- quantile(finalDf[,5])
     projValue1stQuant <- projValueQuants[2]
@@ -65,13 +65,13 @@ projectNBA <- function(modelDate = NULL, slate = NULL, exclude = NULL, cookie = 
     finalDf <- separate(data = finalDf, col = Projected_Ownership, into = c("minOWN", "maxOWN"))
     finalDf <- na.zero(finalDf)
     finalDfChar <- sapply(finalDf,as.character)
-
+    
     write.csv(finalDfChar, file = paste0("~/Desktop/NBA_Daily/",modelDate,slate,"Projections.csv"))
     scoreQuantiles <- quantile(readCSV$Score)
     scoreThirdQuant <- scoreQuantiles[4]
     finalDf3rdQuartileScoreOnly <- finalDf %>% filter(Score>=scoreThirdQuant) %>% arrange(desc(Projection))
     finalDf3rdQuartileScoreOnlyChar <- sapply(finalDf3rdQuartileScoreOnly,as.character)
-
+    
     write.csv(finalDf3rdQuartileScoreOnlyChar, file = paste0("~/Desktop/NBA_Daily/",modelDate,slate,"Projections3rdQuantileOnly.csv"))
     projValueQuants <- quantile(finalDf[,5])
     projValue1stQuant <- projValueQuants[2]
